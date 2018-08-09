e09812575c2d6c500d927a5487611cee46007720
njleonzhang <scream3616@sina.com>
2018-08-09 16:52:55 +0800

    init

---
+++ index.ts
@@ -0,0 +1,16 @@
+export default function init(screenWidthByDesign: number = 1920, screenRatioByDesign: number = 16 / 9) {
+  let docEle = document.documentElement
+
+  function setHtmlFontSize() {
+      var screenRatio = docEle.clientWidth / docEle.clientHeight;
+      var fontSize = (screenRatio > screenRatioByDesign
+          ? (screenRatioByDesign / screenRatio)
+          : 1) * docEle.clientWidth / 10;
+
+      docEle.style.fontSize = fontSize.toFixed(3) + "px";
+  }
+
+  setHtmlFontSize()
+
+  window.addEventListener('resize', setHtmlFontSize)
+}
